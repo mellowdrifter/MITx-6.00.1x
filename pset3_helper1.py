e73fff8228ee03f9d25098ba61e536eebcd95803
mellowdrifter <mellow.drifter@gmail.com>
2015-09-13 18:16:08 +0100

    Create pset3_helper1.py

---
+++ pset3_helper1.py
@@ -0,0 +1,22 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in
+    lettersGuessed; False otherwise
+    '''
+    if len(secretWord) == 1 and secretWord in lettersGuessed:
+        return True
+    else:
+        return secretWord[0] in lettersGuessed \
+            and isWordGuessed(secretWord[1:], lettersGuessed)
+
+
+secretWord = 'apple'
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+print isWordGuessed(secretWord, lettersGuessed)
+
+
+secretWord = 'apple'
+lettersGuessed = ['l', 'i', 'k', 'p', 'a', 'e']
+print isWordGuessed(secretWord, lettersGuessed)
